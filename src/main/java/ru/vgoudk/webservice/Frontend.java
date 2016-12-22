8f6bd341deb2d4f44e23e48076cb1e69b21e9db9
Victor Goudkov <vgoudk@gmail.com>
2016-12-22 20:39:01 +0300

    Add servlet - redirect to bash.im

---
+++ src/main/java/ru/vgoudk/webservice/Frontend.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.vgoudk.webservice;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author VGoudkov
+ */
+public class Frontend extends HttpServlet{
+private String login = "";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //super.doGet(req, resp); //To change body of generated methods, choose Tools | Templates.
+        resp.sendRedirect("http://bash.im");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //super.doPost(req, resp); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
--- src/main/java/ru/vgoudk/webservice/Step_1_4__7.java
+++ src/main/java/ru/vgoudk/webservice/Step_1_4__7.java
@@ -6,6 +6,8 @@
 package ru.vgoudk.webservice;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 /**
  *
@@ -15,8 +17,13 @@ public class Step_1_4__7 {
 
     public static void main(String[] args) throws Exception {
 
-        Server server = new Server(8080);
+        Frontend frontend = new Frontend();
         
+        Server server = new Server(8080);
+        ServletContextHandler context =
+            new ServletContextHandler( ServletContextHandler.SESSIONS);
+        server.setHandler(context);
+        context.addServlet(new ServletHolder( frontend), "/authform");
         server.start();
         server.join();
         
