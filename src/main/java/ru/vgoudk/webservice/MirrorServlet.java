22d75686c47b56afd84e02a7c0e4548adf3b89c3
Victor Goudkov <vgoudk@gmail.com>
2016-12-26 20:36:56 +0300

    WIP page.html

--- src/main/java/ru/vgoudk/webservice/MirrorServlet.java
+++ src/main/java/ru/vgoudk/webservice/MirrorServlet.java
@@ -21,7 +21,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 enum PageVariablesType {
     METHOD("method"),
-    URL("url"),
+    URL("URL"),
     PATHINFO("pathInfo"),
     SESSIONID("sessionId"),
     PARAMETERS("parameters");
@@ -39,15 +39,25 @@ enum PageVariablesType {
 
 public class MirrorServlet extends HttpServlet {
 
-    final String MESSAGE_FIELD = "message";
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         
-        //resp.getWriter().println(PageGenerator.getInstance().getPage("", null));
-        resp.getWriter().println( getParametersMap(req));
         resp.setContentType(PageGenerator.CONTENT_TYPE);
-        resp.setStatus(HttpServletResponse.SC_OK);
+
+        String message = req.getParameter( PageGenerator.MESSAGE_FIELD);
+        if ( message == null || message.isEmpty()){
+            resp.setStatus( HttpServletResponse.SC_FORBIDDEN);
+        }
+        else{
+            resp.setStatus( HttpServletResponse.SC_OK);
+        }
+        
+        Map<String, Object> paraMap = getFreemakerParametersMap(req);
+        paraMap.put( PageGenerator.MESSAGE_FIELD, message == null ? "" : message);
+        
+        //resp.getWriter().println(getTypedParametersMap(req));
+        resp.getWriter().println(PageGenerator.getInstance().getPage("page.html", paraMap));
+        
     }
 
     @Override
@@ -55,14 +65,23 @@ public class MirrorServlet extends HttpServlet {
         super.doPost(req, resp); //To change body of generated methods, choose Tools | Templates.
     }
 
-    protected Map<PageVariablesType, String> getParametersMap(HttpServletRequest req) {
+    protected Map<PageVariablesType, String> getTypedParametersMap(HttpServletRequest req) {
         Map< PageVariablesType, String> vars = new HashMap<>();
         vars.put(PageVariablesType.METHOD, req.getMethod());
         vars.put(PageVariablesType.URL, req.getRequestURL().toString());
         vars.put(PageVariablesType.PATHINFO, req.getPathInfo());
         vars.put(PageVariablesType.SESSIONID, req.getSession().getId());
         vars.put(PageVariablesType.PARAMETERS, req.getParameterMap().toString());
-        vars.put(PageVariablesType.PARAMETERS, req.getParameterMap().toString());
+        return vars;
+    }
+
+    protected Map< String, Object> getFreemakerParametersMap(HttpServletRequest req) {
+        Map<  String, Object> vars = new HashMap<>();
+        vars.put(PageVariablesType.METHOD.toString(), req.getMethod());
+        vars.put(PageVariablesType.URL.toString(), req.getRequestURL().toString());
+        vars.put(PageVariablesType.PATHINFO.toString(), req.getPathInfo());
+        vars.put(PageVariablesType.SESSIONID.toString(), req.getSession().getId());
+        vars.put(PageVariablesType.PARAMETERS.toString(), req.getParameterMap().toString());
         return vars;
     }
 
--- src/main/java/ru/vgoudk/webservice/PageGenerator.java
+++ src/main/java/ru/vgoudk/webservice/PageGenerator.java
@@ -5,6 +5,13 @@
  */
 package ru.vgoudk.webservice;
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.util.Map;
 
 /**
@@ -14,9 +21,11 @@ import java.util.Map;
 public class PageGenerator {
 
     public static final String CONTENT_TYPE = "text/html;charset=utf-8";
-    
-    
+    public static final String HTML_DIR = "templates";
+    public static final String MESSAGE_FIELD = "message";
+
     private static PageGenerator generator;
+    private final Configuration cfg;
 
     public synchronized static PageGenerator getInstance() {
         if (generator == null) {
@@ -25,10 +34,24 @@ public class PageGenerator {
         return generator;
     }
 
-    public String getPage(String fileName, Map<String, Object> data) {
+    public String getHelloPage(String fileName, Map<String, Object> data) {
         return "Hello from PageGenerator!";
     }
-    
-    
-    
+
+    public String getPage(String fileName, Map<String, Object> data) {
+        Writer stream = new StringWriter();
+
+        try {
+            Template template = cfg.getTemplate(HTML_DIR + File.separator + fileName);
+            template.process(data, stream);
+        } catch (IOException | TemplateException e) {
+            e.printStackTrace();
+        }
+        return stream.toString();
+    }
+
+    public PageGenerator() {
+        cfg = new Configuration();
+    }
+
 }
